f5626bc32f0192bf0a75ba360d163dfb84f662cc
Mel Peto <melanie.peto@gmail.com>
2022-05-30 17:04:27 -0400

    updated instructions at top

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// create a legend that hardcodes each mood to a certain one of the 10 hues available in the xColors API - red pink purple navy blue aqua green lime yellow orange
-// make 10 buttons on the screen, each with a 'mood' listed
-// user chooses a mood by clicking on button
-// make an event listener for the 'click' of the button
-// record which mood was chosen
-// display a [card/box/whatever] showing a random color of the chosen mood and a description
-// make a reset button to remove the current color displayed
-
-//Stretch goals:
-//Incorporate another API e.g. caas (cat as a service): instead of a blank color card, use the colour as a semi-transparent filter over a random cat, with text pertaining to the chosen mood
-
-
-// namespace object:
 const colorApp = {};
 
 colorApp.paragraphs = {
@@ -70,7 +57,6 @@ colorApp.getColors = (userChoice) => {
     const colorApiUrl = new URL(colorApp.endpoint);
     colorApiUrl.search = new URLSearchParams({
         hex: userChoice,
-        // mode: 'monochrome-light'
     })
     fetch(colorApiUrl)
         .then((response) => {
